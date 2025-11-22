2778b4bc27c5c86557b563e0dc35b61b5aa9aeb8
alexandresilvaq22-svg <alexandresilvaq22@gmail.com>
2025-11-21 22:12:58 -0300

    Correção de erro

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",  // ← importante para Vercel
+  plugins: [react()],
   build: {
-    outDir: "dist"
+    outDir: 'dist',
   }
 })
